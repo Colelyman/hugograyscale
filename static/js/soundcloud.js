ddfc6e3845e8be9c9022630b787f9d34b6936295
Cole Lyman <cole@colelyman.com>
2018-11-29 14:34:52 -0700

    Removed jQuery from getSoundCloudEmbed

--- static/js/soundcloud.js
+++ static/js/soundcloud.js
@@ -1,18 +1,16 @@
-(function($) {
-    var getSoundCloudEmbed = function (url, id) {
-        var res = $.ajax({
-            type: 'GET',
-            dataType: 'json',
-            url: 'https://soundcloud.com/oembed',
-            data: {
-                format: 'json',
-                url: url,
-                iframe: 'true'
-            },
-            async: false
-        }).responseText
-        console.log(res)
-        var data = $.parseJSON(res)
-        $('#soundcloud-' + id).innerHTML(data.html)
-    }
-})(jQuery);
+var getSoundCloudEmbed = function (url, id) {
+    var res = $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        url: 'https://soundcloud.com/oembed',
+        data: {
+            format: 'json',
+            url: url,
+            iframe: 'true'
+        },
+        async: false
+    }).responseText
+    console.log(res)
+    var data = $.parseJSON(res)
+    $('#soundcloud-' + id).innerHTML(data.html)
+}
